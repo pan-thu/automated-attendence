d71f5ba9bf3e980467564a82313462b40dfaac0e
pan-thu <panthu200@gmail.com>
2025-10-11 22:32:42 +0700

    fix: use correct app environment in widget test

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -1,30 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:client/core/config/app_environment.dart';
+import 'package:client/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:client/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('App bootstrap smoke test', (WidgetTester tester) async {
+    final appEnvironment = AppEnvironment();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Build the app and trigger a frame
+    await tester.pumpWidget(AppBootstrap(environment: appEnvironment));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that splash screen is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
